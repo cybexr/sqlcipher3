04729141492672bbfc08f8c4fa55e34ce025bc78
cybexr <cybexr@github>
2025-07-19 16:54:02 +0800

    ygg-sqlcipher-wheels

--- setup.py
+++ setup.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
     setup(
         # With pyproject.toml, all are not necessary except ext_modules
         # However, they are kept for building python 3.6 wheels
-        name="sqlcipher3-wheels",
+        name="ygg-sqlcipher3-wheels",
         version="0.5.4.post0",
         package_dir={"sqlcipher3": "sqlcipher3"},
         packages=["sqlcipher3"],
